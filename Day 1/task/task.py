1e2956050293914b6fdeb1e09da094c24f403fce
Abdulaziz Abdullahi <ab9laziz@gmail.com>
2026-06-18 21:07:11 +0300

    Day one complete

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator!')
+city=input('Which city did you grow up?\n')
+pet_name=input('What is your pet name?\n')
+band_name=city + " " + pet_name
+print('Your band name could be: ' + band_name)
